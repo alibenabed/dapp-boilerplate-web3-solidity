ac3986ca09d801e80decb6305f8664e2c3c22df5
Hussein Ait Lahcen <hussein.aitlahcen@gmail.com>
2022-06-19 12:17:53 +0200

    mise à jour

--- src/components/App.js
+++ src/components/App.js
@@ -3,99 +3,117 @@ import Web3 from 'web3';
 // import logo from '../logo.png';
 import './App.css';
 import Marketplace from '../abis/Marketplace.json';
-import Navbar from './Navbar'
+import Navbar from './Navbar';
 import Main from './Main';
 
 
 class App extends Component {
 
-  async componentWillMount() {
-    await this.loadWeb3()
-    await this.loadBlockchainData()
-  }
-
-  async loadWeb3() {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
-    }
-  }
-
-  async loadBlockchainData() {
-    const web3 = window.web3
-    // Load account
-    const accounts = await web3.eth.getAccounts()
-    console.log(accounts)
-    this.setState({ account: accounts[0] })
-    const networkId = await web3.eth.net.getId()
-    console.log(networkId)
-    const networkData = Marketplace.networks[networkId]
-    console.log(networkData)
-    if(networkData) {
-      const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
-      console.log(marketplace)
-      this.setState({ marketplace })
-      // eslint-disable-next-line
-      const productCount = await marketplace.methods.productCount().call()
-      console.log(productCount.toString())
-      this.setState({ loading: false})
-    } else {
-      window.alert('Marketplace contract not deployed to detected network.')
-    }
-  }
-  
-  constructor(props) {
-    super(props)
-    this.state = {
-      account: '',
-      productCount: 0,
-      products: [],
-      loading: true
+    async componentWillMount() {
+        await this.loadWeb3();
+        await this.loadBlockchainData();
     }
 
-    this.createProduct = this.createProduct.bind(this)
-    
-  }
+    async loadWeb3() {
+        if (window.ethereum) {
+            window.web3 = new Web3(window.ethereum);
+            await window.ethereum.enable();
+        }
+        else if (window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider);
+        }
+        else {
+            window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+        }
+    }
 
-  createProduct(name, price) {
-    this.setState({ loading: true })
-    this.state.marketplace.methods.createProduct(name, price).send({ from: this.state.account })
-    .once('receipt', (receipt) => {
-      this.setState({ loading: false })
-    })
-  }
+    async loadBlockchainData() {
+        const web3 = window.web3;
+        // Load account
+        const accounts = await web3.eth.getAccounts();
+        console.log(accounts);
+        const currentAccount = accounts[0];
+        this.setState({ account: currentAccount });
+        const networkId = await web3.eth.net.getId();
+        console.log(networkId);
+        const networkData = Marketplace.networks[networkId];
+        console.log(networkData);
+        if (networkData) {
+            const marketplace = new web3.eth.Contract(Marketplace.abi, networkData.address);
+            console.log(marketplace);
+            this.setState({ marketplace });
+            this.loadProducts();
+            this.setState({ loading: false });
+        } else {
+            window.alert('Marketplace contract not deployed to detected network.');
+        }
+    }
 
-  purchaseProduct(id, price) {
-    this.setState({ loading: true })
-    this.state.marketplace.methods.purchaseProduct(id).send({ from: this.state.account, value: price })
-    .once('receipt', (receipt) => {
-      this.setState({ loading: false })
-    })
-  }
+    constructor(props) {
+        super(props);
 
-  render() {
-    return (
-      <div>
-        <Navbar account={this.state.account} />
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex">
-              { this.state.loading
-                ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
-                : <Main createProduct={this.createProduct} />
-              }
-            </main>          
-          </div>
-        </div>
-      </div>
-    );
-  }
+        this.state = {
+            account: '',
+            productCount: 0,
+            products: [],
+            loading: true
+        };
+
+        this.createProduct = this.createProduct.bind(this);
+        this.purchaseProduct = this.purchaseProduct.bind(this);
+        this.loadProducts = this.loadProducts.bind(this);
+    }
+
+    async loadProducts() {
+        // this.setState({ loading: true });
+        // const productCount = await this.state.marketplace.methods.productCount().call();
+        // console.log(productCount);
+        // const products = [];
+        // for (let i = 1; i <= productCount; i++) {
+        //     const product = await this.state.marketplace.methods.products(i).call();
+        //     console.log(product);
+        //     products.push(product);
+        // }
+        // this.setState({ products });
+        // this.setState({ loading: false });
+    }
+
+    createProduct(name, price) {
+        console.log("Creating product: " + name + ", price: " + price);
+        this.setState({ loading: true });
+        this.state.marketplace.methods.createProduct(name, price).send({ from: this.state.account, value: price })
+            .once('receipt', (receipt) => {
+                this.loadProducts();
+            })
+    }
+
+    purchaseProduct(id, price) {
+        console.log("Puchasing: " + id + ", price: " + price);
+        this.setState({ loading: true });
+        this.state.marketplace.methods.purchaseProduct(id).send({ from: this.state.account, value: price })
+            .once('receipt', (receipt) => {
+                this.loadProducts();
+            })
+    }
+
+    render() {
+        console.log(this.state)
+        return (
+            <div>
+                <Navbar account={this.state.account} />
+                <div className="container-fluid mt-5">
+                    <div className="row">
+                        <main role="main" className="col-lg-12 d-flex">
+                            {this.state.loading
+                                ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
+                                : <Main createProduct={this.createProduct} purchaseProduct={this.purchaseProduct} products={this.state.products} />
+                            }
+                        </main>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,73 +1,66 @@
 import React, { Component } from 'react';
 
-class Main extends Component {  
+class Main extends Component {
     render() {
-    return (
-      <div id="content">
-        <h1>Add Product</h1>
-        <form onSubmit={(event) => {
-          event.preventDefault()
-          const name = this.productName.value
-          const price = window.web3.utils.toWei(this.productPrice.value.toString(), 'Ether')
-          this.props.createProduct(name, price)
-        }}>
-          <div className="form-group mr-sm-2">
-            <input
-              id="productName"
-              type="text"
-              ref={(input) => { this.productName = input }}
-              className="form-control"
-              placeholder="Product Name"
-              required />
-          </div>
-          <div className="form-group mr-sm-2">
-            <input
-              id="productPrice"
-              type="text"
-              ref={(input) => { this.productPrice = input }}
-              className="form-control"
-              placeholder="Product Price"
-              required />
-          </div>
-          <button type="submit" className="btn btn-primary">Add Product</button>
-        </form>
-        <p> </p>
-        <h2>Buy Product</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Name</th>
-              <th scope="col">Price</th>
-              <th scope="col">Owner</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody id="productList">
-            <tr>
-              <th scope="row">1</th>
-              <td>iPhone x</td>
-              <td>1 Eth</td>
-              <td>0x39C7BC5496f4eaaa1fF75d88E079C22f0519E7b9</td>
-              <td><button className="buyButton">Buy</button></td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Macbook Pro</td>
-              <td>3 eth</td>
-              <td>0x39C7BC5496f4eaaa1fF75d88E079C22f0519E7b9</td>
-              <td><button className="buyButton">Buy</button></td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Airpods</td>
-              <td>0.5 eth</td>
-              <td>0x39C7BC5496f4eaaa1fF75d88E079C22f0519E7b9</td>
-              <td><button className="buyButton">Buy</button></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}export default Main;
+        return (
+            <div id="content">
+                <h1>Add Product</h1>
+                <form onSubmit={(event) => {
+                    event.preventDefault()
+                    const name = this.productName.value
+                    const price = window.web3.utils.toWei(this.productPrice.value.toString(), 'Ether')
+                    this.props.createProduct(name, price)
+                }}>
+                    <div className="mb-3">
+                        <input
+                            id="productName"
+                            type="text"
+                            ref={(input) => { this.productName = input }}
+                            className="form-control"
+                            placeholder="Product Name"
+                            required />
+                    </div>
+                    <div className="mb-3">
+                        <input
+                            id="productPrice"
+                            type="text"
+                            ref={(input) => { this.productPrice = input }}
+                            className="form-control"
+                            placeholder="Product Price"
+                            required />
+                    </div>
+                    <button type="submit" className="mb-3 btn btn-primary">Add Product</button>
+                </form>
+                <p> </p>
+                <h2>Buy Product</h2>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Owner</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody id="productList">
+                        {this.props.products.map(product =>
+                            <tr key={product.id}>
+                                <th scope="row">{product.id}</th>
+                                <td>{product.name}</td>
+                                <td>{window.web3.utils.fromWei(product.price, 'Ether')} ETH</td>
+                                <td>{product.owner}</td>
+                                {!product.purchased && <td><button className="buyButton"
+                                    onClick={(event) => {
+                                        event.preventDefault()
+                                        this.props.purchaseProduct(window.web3.utils.toBN(product.id), window.web3.utils.toBN(product.price))
+                                    }}>Buy</button></td>}
+                            </tr>
+                        )
+                        }
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+} export default Main;
